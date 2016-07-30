24d7f1f321a080274ce32167c434da96f7a27a2b
Hank Wang <drapho@gmail.com>
2016-07-30 20:22:03 +0800

    update program structure

--- app.py
+++ app.py
@@ -2,20 +2,26 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-url = "http://invoice.etax.nat.gov.tw/"
 
-try:
-    headers = {
-        "Accept": "text/html",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.160 Safari/537.22"
-    }
-    request = urllib2.Request(url, headers=headers)
-    response = urllib2.urlopen(request).read()
-except (urllib2.HTTPError, urllib2.URLError) as e:
-    print e
-    exit(1)
+def main():
+    url = "http://invoice.etax.nat.gov.tw/"
 
-soup = BeautifulSoup(response, "html.parser")
-# 我只需要這個月的，所以只取前四個Row
-for row in soup.findAll("span", {"class": "t18Red"})[:4]:
-    print row.renderContents()
+    try:
+        headers = {
+            "Accept": "text/html",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.160 Safari/537.22"
+        }
+        request = urllib2.Request(url, headers=headers)
+        response = urllib2.urlopen(request).read()
+    except (urllib2.HTTPError, urllib2.URLError) as e:
+        print e
+        exit(1)
+
+    soup = BeautifulSoup(response, "html.parser")
+    # 我只需要這個月的，所以只取前四個Row
+    for row in soup.findAll("span", {"class": "t18Red"})[:4]:
+        print row.renderContents()
+
+
+if __name__ == '__main__':
+    main()
