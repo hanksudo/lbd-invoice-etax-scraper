58d3c073fbd5202021f9c46cf568cfaa2b7164ab
Hank Wang <drapho@gmail.com>
2020-03-12 00:40:26 +0900

    Change to Python 3 and update bs4 version

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # encoding=utf-8
-import urllib2
+from urllib.request import urlopen, Request
+from urllib.error import URLError, HTTPError
 from bs4 import BeautifulSoup
 
 
@@ -11,16 +12,25 @@ def main():
             "Accept": "text/html",
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.160 Safari/537.22"
         }
-        request = urllib2.Request(url, headers=headers)
-        response = urllib2.urlopen(request).read()
-    except (urllib2.HTTPError, urllib2.URLError) as e:
-        print e
+        request = Request(url, headers=headers)
+        response = urlopen(request).read()
+    except (HTTPError, URLError) as e:
+        print(e)
         exit(1)
 
     soup = BeautifulSoup(response, "html.parser")
-    # 我只需要這個月的，所以只取前四個Row
-    for row in soup.findAll("span", {"class": "t18Red"})[:4]:
-        print row.renderContents()
+    for row in soup.find(id="area1").find("table").find_all("tr"):
+        cols = row.find_all("td")
+        if not cols:
+            continue
+        
+        numbers_col = cols[1].find("span", {"class": "t18Red"})
+        if not numbers_col:
+            continue
+            
+        print(cols[0].text)
+        for number in numbers_col.text.split("、"):
+            print("\t{}".format(number))
 
 
 if __name__ == "__main__":
